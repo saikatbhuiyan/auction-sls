cd348c0021a2eb63bb980c04fffef7db64b4cd8e
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-04-29 20:55:27 +0600

    Validate auction ownership

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -7,6 +7,13 @@ import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this auction!`);
+  }
+
   const auction = await getAuctionById(id);
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
